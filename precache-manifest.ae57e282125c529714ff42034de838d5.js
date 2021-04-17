da2efa2709672ae28d13297d525eceb2fbd8803c
Prajjwal Singh <prajjwal875.com>
2021-04-17 23:51:48 +0530

    Updates

---
+++ precache-manifest.ae57e282125c529714ff42034de838d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514d4d8a4a9e946cb86ff93ec3a87587",
+    "url": "/TODO-app/index.html"
+  },
+  {
+    "revision": "6dac9360b2744beed3e9",
+    "url": "/TODO-app/static/css/2.94f0939c.chunk.css"
+  },
+  {
+    "revision": "6dac9360b2744beed3e9",
+    "url": "/TODO-app/static/js/2.f0b614d6.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/TODO-app/static/js/2.f0b614d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06b4c43159693089d8d",
+    "url": "/TODO-app/static/js/main.a63ee051.chunk.js"
+  },
+  {
+    "revision": "d0d03a41e97f727d6f52",
+    "url": "/TODO-app/static/js/runtime-main.a16d6c55.js"
+  }
+]);
